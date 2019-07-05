a19da489a053b2e2530348a440fb79c0ee56c5aa
sicypress <sly329@aliyun.com>
2019-07-05 15:21:32 +0800

    adminMapper

--- src/main/java/com/cskaoyan14th/mapper/AdminMapper.java
+++ src/main/java/com/cskaoyan14th/mapper/AdminMapper.java
@@ -13,5 +13,19 @@ public interface AdminMapper {
     List<Admin> queryAdminAll();
 
     Admin selectByPrimaryKey(int i);
+    
+    int insertAdmin(@Param("adm") Admin admin);
+
+    Admin queryAdminByUsername(@Param("user") Admin username);
+
+    int updatetAdmin(@Param("admin") Admin admin);
+
+    Admin queryAdminById(@Param("aid") Admin id);
+
+    int deleteAdmin(@Param("admin") Admin admin);
+
+    List<Admin> queryAdminAllByUsername(@Param("user") String username);
+
+
 
 }
